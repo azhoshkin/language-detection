42416a289264cf28c924979ba0db9cc06fc51c6b
alexander_zhoshkin <alexander_zhoshkin@epam.com>
2017-04-19 18:21:34 +0300

    Class re-factoring

---
+++ LanguageDetection/DetectedLanguage.cs
@@ -0,0 +1,9 @@
+namespace LanguageDetection
+{
+    public class DetectedLanguage
+    {
+        public string Language { get; set; }
+        public double Probability { get; set; }
+    }
+
+}
---
+++ LanguageDetection/ILanguageDetector.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace LanguageDetection
+{
+    interface ILanguageDetector
+    {
+        double Alpha { get; set; }
+        int? RandomSeed { get; set; }
+        int Trials { get; set; }
+        int NGramLength { get; set; }
+        int MaxTextLength { get; set; }
+        double AlphaWidth { get; set; }
+        int MaxIterations { get; set; }
+        double ProbabilityThreshold { get; set; }
+        double ConvergenceThreshold { get; set; }
+        int BaseFrequency { get; set; }
+
+        void AddAllLanguages();
+        void AddLanguages(params string[] languages);
+        string Detect(string text);
+        IEnumerable<DetectedLanguage> DetectAll(string text);
+    }
+}
